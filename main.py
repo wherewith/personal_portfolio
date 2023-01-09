7164eca6c05486799831b9c99811d86ab6c65559
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2023-01-09 15:22:31 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/projects')
+def projects_page():
+    return render_template("projects.html")
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
